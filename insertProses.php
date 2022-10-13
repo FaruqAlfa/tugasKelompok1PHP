6c1438224be0b81dff29e3c286db3d8ebd91c7b1
nicoco1173 <rikurunico@gmail.com>
2022-10-13 10:53:38 +0700

    Fix

--- insertProses.php
+++ insertProses.php
@@ -4,7 +4,7 @@ $nama_buku = $_GET['nama_buku'];
 $stok_buku = $_GET['stok_buku'];
 $harga_buku = $_GET['harga_buku'];
 
-$sql = "INSERT INTO gudang (nama_buku, stok_buku, harga_buku) VALUES ('$id_buku', '$nama_buku', '$stok_buku', '$harga_buku')";
+$sql = "INSERT INTO gudang (nama_buku, stok_buku, harga_buku) VALUES ('$nama_buku', '$stok_buku', '$harga_buku')";
 
 if (mysqli_query($connect, $sql)) {
     echo "Sukses Menambahkan Data baru";
